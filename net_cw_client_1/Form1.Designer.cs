8b8701e2b17340e67586fc828ef38c083a2de81e
Dimonch1k <leskiv-dmytro@lyc2.org>
2024-09-16 20:04:26 +0300

    common project with Bogdan

---
+++ net_cw_client_1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace net_cw_client_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernameTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // usernameTB
+            // 
+            this.usernameTB.Location = new System.Drawing.Point(138, 72);
+            this.usernameTB.Name = "usernameTB";
+            this.usernameTB.Size = new System.Drawing.Size(100, 20);
+            this.usernameTB.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Location = new System.Drawing.Point(138, 114);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.Size = new System.Drawing.Size(100, 20);
+            this.passwordTB.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 306);
+            this.Controls.Add(this.passwordTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usernameTB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox usernameTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passwordTB;
+    }
+}
+
--- net_server/Program.cs
+++ net_server/Program.cs
@@ -11,6 +11,9 @@ namespace net_cw_1
 
         static string connectionString = "Server=34.118.84.47;Database=ChatApp;User ID=dmytro;";
 
+        //static string connectionString = ConfigurationManager.ConnectionStrings["DB_ChatApp"].ConnectionString;
+
+
         static void Main(string[] args)
         {
             TcpListener server = new TcpListener(IPAddress.Any, 25564);
@@ -86,6 +89,39 @@ namespace net_cw_1
                         Console.WriteLine($"Error during sending message: {ex.Message}");
                     }
                 }
+            }
+        }
+
+        static void LogConnection(TcpClient client)
+        {
+            string clientAddress = client.Client.RemoteEndPoint.ToString();
+            string query = "INSERT INTO ConnectionLogs (ClientAddress, ConnectionTime) VALUES (@ClientAddress, @ConnectionTime)";
+
+            using (ConnectionString = new(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ClientAddress", clientAddress);
+                cmd.Parameters.AddWithValue("@ConnectionTime", DateTime.Now);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        static void LogDisconnection(TcpClient client)
+        {
+            string clientAddress = client.Client.RemoteEndPoint.ToString();
+            string query = "UPDATE ConnectionLogs SET DisconnectionTime = @DisconnectionTime WHERE ClientAddress = @ClientAddress AND DisconnectionTime IS NULL";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ClientAddress", clientAddress);
+                cmd.Parameters.AddWithValue("@DisconnectionTime", DateTime.Now);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
             }
         }
     }
